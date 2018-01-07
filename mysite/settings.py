2fbec0c7907031d9d5975bbfee74f9438bda2d42
Trybnetic <marc.weitz@onlinehome.de>
2018-01-07 20:19:51 +0100

    fixes settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'monitor'
+    'fachschaftsempfaenger'
 ]
 
 MIDDLEWARE = [
