8e6bde6ed2e75d69f0e276a071e43b46f6d68664
elogy <tim.beckmann@student.uni-tuebingen.de>
2018-01-14 00:37:48 +0100

    adds search for food items

--- fachschaftsempfaenger/admin.py
+++ fachschaftsempfaenger/admin.py
@@ -6,5 +6,11 @@ from .models import Menu, Food
 """
 registers the models concerning the food truck to the Django admin.
 """
-admin.site.register(Food)
+
+
+class FoodAdmin(admin.ModelAdmin):
+    search_fields = ['meal']
+
+
+admin.site.register(Food, FoodAdmin)
 admin.site.register(Menu)
