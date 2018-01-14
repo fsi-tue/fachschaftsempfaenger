0be24c258d03491c836de8ff5c4b54c5afeadfe4
elogy <tim.beckmann@student.uni-tuebingen.de>
2018-01-14 18:08:00 +0100

    removes unneeded docstring for admin

--- fachschaftsempfaenger/admin.py
+++ fachschaftsempfaenger/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Menu, Food
-
-"""
-registers the models concerning the food truck to the Django admin.
-"""
 
 
 class FoodAdmin(admin.ModelAdmin):
