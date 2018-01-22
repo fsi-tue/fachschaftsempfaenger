93e266496900bee16d518a2d4aa8ff740283b8c3
elogy <tim.beckmann@student.uni-tuebingen.de>
2018-01-22 13:20:20 +0100

    turns off debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@f6!(9=6lc45&rt846kspn-&gxi&^(kp&$)y^8rx84r-wzl+ua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
