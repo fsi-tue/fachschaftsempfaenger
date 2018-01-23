db4ed02a6d0695eb934463487ace3c0f2f369b12
elogy <tim.beckmann@student.uni-tuebingen.de>
2018-01-23 13:11:47 +0100

    adds meal price to admin list view

--- fachschaftsempfaenger/admin.py
+++ fachschaftsempfaenger/admin.py
@@ -5,6 +5,7 @@ from .models import Menu, Food, Advertisement
 
 class FoodAdmin(admin.ModelAdmin):
     search_fields = ['meal']
+    list_display = ['meal', 'price']
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
