41877c7975654b20efc3cd4b6f94c6e22b239404
elogy <tim.beckmann@student.uni-tuebingen.de>
2018-01-25 11:51:37 +0100

    reverts to usage of development server

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@f6!(9=6lc45&rt846kspn-&gxi&^(kp&$)y^8rx84r-wzl+ua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/assets')
-# STATICFILES_DIRS = [STATIC_ROOT, ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
