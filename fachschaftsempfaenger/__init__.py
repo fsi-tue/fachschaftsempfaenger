10129addfd5a875555be70107608b5693fd1e7fb
Trybnetic <marc.weitz@onlinehome.de>
2018-02-25 17:49:46 +0100

    bumps version to 1.0.0

--- fachschaftsempfaenger/__init__.py
+++ fachschaftsempfaenger/__init__.py
@@ -11,18 +11,19 @@ delivered using the Django Web Framework.
 __author__ = ('Marc Weitz & Tim Beckmann')
 __author_email__ = 'fachschaftsempfaenger@fsi.uni-tuebingen.de'
 __repository_url__ = 'https://github.com/fsi-tue/fachschaftsempfaenger'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __license__ = 'MIT'
 __description__ = ('Fachschaftsempfaenger is the official infoscreen of the '
                     'student union of '
                     'Computer Science at the university of Tuebingen.')
 __classifiers__=[
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
+    'Framework :: Django',
+    'License :: OSI Approved :: MIT License',
     'Environment :: Web Environment',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: System Administrators',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Framework :: Django',
 ],
