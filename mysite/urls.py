26754a7e18f21c168f476116792ff713d64b1f72
Trybnetic <marc.weitz@onlinehome.de>
2018-03-01 16:47:12 +0100

    removes django stuff

--- mysite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^', include('fachschaftsempfaenger.urls')),
-    url(r'^admin/', admin.site.urls),
-]
