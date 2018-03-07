d19ad93582723d3a70b38b156ab6be8bf42d7b0b
Trybnetic <marc.weitz@onlinehome.de>
2018-03-07 14:01:32 +0100

    reverts previous commit because of the discussion about when to release 1.0.0

--- fachschaftsempfaenger/__init__.py
+++ fachschaftsempfaenger/__init__.py
@@ -11,7 +11,7 @@ delivered using the Django Web Framework.
 __author__ = ('Marc Weitz & Tim Beckmann')
 __author_email__ = 'fachschaftsempfaenger@fsi.uni-tuebingen.de'
 __repository_url__ = 'https://github.com/fsi-tue/fachschaftsempfaenger'
-__version__ = '1.0.1'
+__version__ = '1.0.0'
 __license__ = 'MIT'
 __description__ = ('Fachschaftsempfaenger is the official infoscreen of the '
                     'student union of '
