e2eadcc590d88924aaac2e151688fbf1e8329cb7
Lukas Oertel <git@luoe.dev>
2019-08-06 10:21:13 +0200

    Add paths for static and media files
    So that the files won't be stored in the root of this project and our reverse proxy can serve them directly

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+MEDIA_URL = '/static/media/'
+MEDIA_ROOT = '/static/media/'
