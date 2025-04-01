bbc7978a141f0028e9f665843f6a62e49c9a2c23
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2025-04-01 16:20:54 +0200

    Downgrade to Django 4.2.x (#257)
    * Upgrade to Django 5
    
    * Update Docker
    
    * Replace old setup.py with modern pyproject.toml
    
    * Restyle and rewrite parts of Fachschaftsempfänger
    
    * Downgrade to Django 4.2.20. It has support until April 2026.
    
    * Revert the changes in the docker-compose.yml

--- fachschaftsempfaenger/admin.py
+++ fachschaftsempfaenger/admin.py
@@ -3,27 +3,26 @@ from django.contrib import admin
 from .models import Menu, Food, Advertisement, Member, Mastodon
 
 
-@admin.register(Food)
 class FoodAdmin(admin.ModelAdmin):
     search_fields = ['meal']
     list_display = ['meal', 'price']
 
 
-@admin.register(Advertisement)
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ['id', 'start_date', 'end_date', 'image']
 
 
-@admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name']
     ordering = ['last_name']
 
 
-@admin.register(Mastodon)
 class MastodonAdmin(admin.ModelAdmin):
     list_display = ['visible', 'instance', 'username']
 
 
+admin.site.register(Food, FoodAdmin)
 admin.site.register(Menu)
-
+admin.site.register(Advertisement, AdvertisementAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Mastodon, MastodonAdmin)
